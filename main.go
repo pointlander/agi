29212761e65e3f578eac13c81c9658de70a1db9a
Andrew Snodgrass <pointlander@gmail.com>
2024-12-12 11:33:20 -0600

    Added puzzle loading

--- main.go
+++ main.go
@@ -4,10 +4,56 @@
 
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
 const (
 	// Size is the number of histograms
 	Size = 8
 )
+
+// Example is a learning example
+type Example struct {
+	Input  [][]byte `json:"input"`
+	Output [][]byte `json:"output"`
+}
+
+// Set is a set of examples
+type Set struct {
+	Test  []Example `json:"test"`
+	Train []Example `json:"train"`
+}
+
+// Load loads the data
+func Load() []Set {
+	dirs, err := os.ReadDir("ARC-AGI/data/training/")
+	if err != nil {
+		panic(err)
+	}
+	sets := make([]Set, len(dirs))
+	for i, dir := range dirs {
+		data, err := os.ReadFile("ARC-AGI/data/training/" + dir.Name())
+		if err != nil {
+			panic(err)
+		}
+		err = json.Unmarshal(data, &sets[i])
+		if err != nil {
+			panic(err)
+		}
+	}
+	fmt.Println("loaded", len(sets))
+	test, train := 0, 0
+	for _, set := range sets {
+		test += len(set.Test)
+		train += len(set.Train)
+	}
+	fmt.Println("test", test)
+	fmt.Println("train", train)
+	return sets
+}
 
 // Markov is a markov model
 type Markov [2]byte
@@ -119,5 +165,5 @@ func (m *Mixer) Add(s byte) {
 }
 
 func main() {
-
+	Load()
 }
